5f74ff804be3e72f2f8824d645242b380311e0e6
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-02-13 19:03:17 -0500

    created the task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-from _datetime import datetime
-import uuid
-    """
-    import the modules in the code
-    """
+import datetime
+from uuid import uuid4
+"""
+import the modules in the code
+"""
 
-class BaseModelque:
+
+class BaseModel:
     """
     The main class
     """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self, id=None, created_at=None, update_at=None):
+        """ The constructor """
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        print ("[<{}>] (<{}>) <{}>".format(self.id, self.__dict__)
+        """ the __str__ display the attributes of the class"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                      self.__dict__))
 
     def save(self):
+        """ updates the public instance attribute """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """ a dictionary containing all keys/values """
+        my_dict = self.__dict__
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
+        return my_dict
