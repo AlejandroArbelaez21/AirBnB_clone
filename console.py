cddced4303898831e10b8a4c04c568644acdb7ce
towasme <towasm@gmail.com>
2020-02-19 15:51:15 -0500

    added all, update and destroy methods for the console

--- console.py
+++ console.py
@@ -13,15 +13,14 @@ from models.review import Review
 """
 program console
 """
-#dict_class = {'BaseModel': BaseModel, 'User': User, 'Amenity': Amenity,
-#              'City': City, 'Place': Place, 'Review': Review, 'State': State}
+
+
 list_class = ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]
 
 
 class HBNBCommand(cmd.Cmd):
     """console that contains the entry point of the command interpreter"""
     prompt = '(hbnb) '
-    #    Thomas = (globals()['BaseModel'])
 
     def do_quit(self, line):
         """Quit command to exit the program
@@ -39,7 +38,6 @@ class HBNBCommand(cmd.Cmd):
 
     #    def help(self, line):
     #        """shows the user the different commands the console has"""
-    #
 
     def do_create(self, line):
         args = split(line)
@@ -110,12 +108,31 @@ class HBNBCommand(cmd.Cmd):
         print(new_list)
 
     def do_update(self, line):
+        """method to update the attributes of the classes"""
+        args = split(line)
         if len(line) == 0:
             print("** class name missing **")
-        elif line != 'BaseModel':
+            return False
+        if args[0] in list_class:
+            if len(args) > 1:
+                objs = storage.all()
+                key = args[0] + '.' + args[1]
+                if key in objs:
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            get_attr = storage.all().get(key)
+                            setattr(get_attr, args[2], args[3])
+                            storage.save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
             print("** class doesn't exist **")
-        elif line == 'BaseModel':
-            print("** instance id missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
