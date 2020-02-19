443e843759d62c6bc0d4c8b249b07eb631ee5284
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-02-19 16:49:03 -0500

    fixing up the pep8 and user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,15 +11,14 @@ from models.review import Review
 from models.user import User
 
 
-def test_pep8_conformance_user(self):
-    """Test that we conform to PEP8."""
-    pep8style = pep8.StyleGuide(quiet=True)
-    result = pep8style.check_files(['models/user.py'])
-    self.assertEqual(result.total_errors, 0,
-                     "Found code style errors (and warnings).")
-
-
 class Testuser(unittest.TestCase):
+
+    def test_pep8_conformance_user(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_User(self):
         """
